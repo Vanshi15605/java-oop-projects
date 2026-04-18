f41116b6ef8de2db753d190b86313f91ce066c4a
Vanshi15605 <vanshikasharma15605@gmail.com>
2026-04-18 14:06:21 +0530

    Add Warrior class extending Character with attack method

---
+++ BattleGame/Warrior.java
@@ -0,0 +1,17 @@
+
+class Warrior extends Character implements Attackable
+{
+    static Random r = new Random();
+
+    Warrior(String name,int health)
+    {
+        super(name,health);
+    }
+
+    public int attack()
+    {
+        if(random chance)
+            
+        return 15+r.nextInt(11);
+    }
+}
